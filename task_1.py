e8e8d48bfb5375667edb7cce6f522ae990a009fa
ahmelev <ahmelev@gmail.com>
2021-08-29 01:13:34 +0700

    task_123 update

--- task_1.py
+++ task_1.py
@@ -1,3 +1,10 @@
-print("Hello teacher")
-print("Hello teacher")
-print("Hello teacher")
+duration = int(input("Введите число: "))
+
+day = duration // (60*60*24)
+duration %= (60*60*24)
+hours = duration // (60*60)
+duration %= (60*60)
+minutes = duration // 60
+duration %= 60
+
+print(f"Из введенного числа получается {day} дн {hours} час {minutes} мин {duration} сек")
