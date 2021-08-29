3b5eb637196ed53ebeccc29a6b08875ad3c6bbc9
ahmelev <ahmelev@gmail.com>
2021-08-29 23:01:10 +0700

    update

--- task_3.py
+++ task_3.py
@@ -1,26 +1,35 @@
+#
+# for i in range(1, 101):
+#     if (1 < i % 10 < 5) or (i % 10 == 0):
+#         print(f"{i} процента")
+#     elif 5 <= i % 10 <= 9:
+#         print(f"{i} процентов")
+#     else:
+#         print(f"{i} процент")
+
+
 for i in range(1, 101):
-    if (1 < i < 5) \
-            or (21 < i < 25) \
-            or (31 < i < 35) \
-            or (41 < i < 45) \
-            or (51 < i < 55) \
-            or (61 < i < 65) \
-            or (71 < i < 75) \
-            or (81 < i < 85) \
-            or (91 < i < 95):
+    if (5 <= i % 10 <= 9) or (10 <= i <= 14):
         print(f"{i} процента")
-    elif 5 <= i < 21 \
-            or (25 <= i < 31) \
-            or (35 <= i < 41) \
-            or (45 <= i < 51) \
-            or (55 <= i < 61) \
-            or (65 <= i < 71) \
-            or (75 <= i < 81) \
-            or (85 <= i < 91) \
-            or 95 <= i:
+    elif (1 < i % 10 < 5) or (i % 10 == 0):
         print(f"{i} процентов")
     else:
         print(f"{i} процент")
+
+
+# percent = input("Введите целое число: ")
+# if isinstance(percent, int):
+#     if percent > 0:
+#         if (1 < percent % 10 < 5) or (percent % 10 == 0):
+#             print(f"{percent} процента")
+#         elif 5 <= percent % 10 <= 9:
+#             print(f"{percent} процентов")
+#         else:
+#             print(f"{percent} процент")
+#     else:
+#         print("Вы ввели отрицательное число!")
+# else:
+#     print("Вы ввели не число число или не целое число!")
 
 # Реализовать склонение слова «процент» во фразе «N процентов». Вывести эту фразу на экран отдельной строкой для
 # каждого из чисел в интервале от 1 до 100:
@@ -32,30 +41,3 @@ for i in range(1, 101):
 # 6 процентов
 # ...
 # 100 процентов
-
-percent = int(input("Введите целое число от 1 до 100"))
-if 0 < percent < 101:
-    if (1 < percent < 5) \
-            or (21 < percent < 25) \
-            or (31 < percent < 35) \
-            or (41 < percent < 45) \
-            or (51 < percent < 55) \
-            or (61 < percent < 65) \
-            or (71 < percent < 75) \
-            or (81 < percent < 85) \
-            or (91 < percent < 95):
-        print(f"{percent} процента")
-    elif 5 <= percent < 21 \
-            or (25 <= percent < 31) \
-            or (35 <= percent < 41) \
-            or (45 <= percent < 51) \
-            or (55 <= percent < 61) \
-            or (65 <= percent < 71) \
-            or (75 <= percent < 81) \
-            or (85 <= percent < 91) \
-            or 95 <= percent:
-        print(f"{percent} процентов")
-    else:
-        print(f"{percent} процент")
-else:
-    print("Вы ввели некорректное число!")
