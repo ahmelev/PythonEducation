cbd4ed2f3d450ceb0d8d420f51a8eccb5f8142b0
ahmelev <89439071+ahmelev@users.noreply.github.com>
2021-08-30 00:29:58 +0700

    Revert "lesson_1"

--- task_1.py
+++ task_1.py
@@ -1,14 +1,3 @@
-duration = int(input("Введите число: "))
-
-day = duration // (60 * 60 * 24)
-duration %= (60 * 60 * 24)
-hours = duration // (60 * 60)
-duration %= (60 * 60)
-minutes = duration // 60
-duration %= 60
-
-print(f"Из введенного числа получается {day} дн {hours} час {minutes} мин {duration} сек")
-
-# Реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах: до
-# минуты: <s> сек; до часа: <m> мин <s> сек; до суток: <h> час <m> мин <s> сек; * в остальных случаях: <d> дн <h> час
-# <m> мин <s> сек.
+print("Hello teacher")
+print("Hello teacher")
+print("Hello teacher")
