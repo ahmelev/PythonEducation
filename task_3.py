d3026cdc0926e5c53342cbcc8fc06f1bba74d56c
ahmelev <ahmelev@gmail.com>
2021-08-30 00:35:25 +0700

    update

--- task_3.py
+++ task_3.py
@@ -1,11 +1,3 @@
-for i in range(1, 101):
-    if (1 < i % 10 < 5) and (i != 12) and (i != 13) and (i != 14):
-        print(f"{i} процента")
-    elif (i % 10 == 1) and (i != 11):
-        print(f"{i} процент")
-    else:
-        print(f"{i} процентов")
-
 # percent = int(input("Введите целое число: "))
 # if percent > 0:
 #     if (1 < percent % 10 < 5) and (percent != 12) and (percent != 13) and (percent != 14):
@@ -27,3 +19,11 @@ for i in range(1, 101):
 # 6 процентов
 # ...
 # 100 процентов
+
+for i in range(1, 101):
+    if (1 < i % 10 < 5) and (i != 12) and (i != 13) and (i != 14):
+        print(f"{i} процента")
+    elif (i % 10 == 1) and (i != 11):
+        print(f"{i} процент")
+    else:
+        print(f"{i} процентов")
