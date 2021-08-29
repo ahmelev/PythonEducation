196a871a54e8cb4ba593eedb0b2e341ee473dc89
ahmelev <ahmelev@gmail.com>
2021-08-30 00:54:01 +0700

    update

--- task_3.py
+++ task_3.py
@@ -1,14 +1,3 @@
-# percent = int(input("Введите целое число: "))
-# if percent > 0:
-#     if (1 < percent % 10 < 5) and (percent != 12) and (percent != 13) and (percent != 14):
-#         print(f"{percent} процента")
-#     elif (percent % 10 == 1) and (percent != 11):
-#         print(f"{percent} процент")
-#     else:
-#         print(f"{percent} процентов")
-# else:
-#     print("Вы ввели отрицательное число!")
-
 # Реализовать склонение слова «процент» во фразе «N процентов». Вывести эту фразу на экран отдельной строкой для
 # каждого из чисел в интервале от 1 до 100:
 # 1 процент
@@ -27,3 +16,14 @@ for i in range(1, 101):
         print(f"{i} процент")
     else:
         print(f"{i} процентов")
+
+# percent = int(input("Введите целое число: "))
+# if percent > 0:
+#     if (1 < percent % 10 < 5) and (percent != 12) and (percent != 13) and (percent != 14):
+#         print(f"{percent} процента")
+#     elif (percent % 10 == 1) and (percent != 11):
+#         print(f"{percent} процент")
+#     else:
+#         print(f"{percent} процентов")
+# else:
+#     print("Вы ввели отрицательное число!")
