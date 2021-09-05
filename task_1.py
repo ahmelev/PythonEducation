66ef4d32032f196c1a5e9c40f0cb1a1d4435b84e
ahmelev <ahmelev@gmail.com>
2021-09-05 20:41:37 +0700

    update

--- task_1.py
+++ task_1.py
@@ -3,3 +3,29 @@
 
 # Подумайте, как и где лучше хранить информацию, необходимую для перевода: какой тип данных выбрать, в теле функции
 # или снаружи.
+
+translate_dict = \
+    {
+        "Zero": "Ноль",
+        "One": "Один",
+        "Two": "Два",
+        "Three": "Три",
+        "Four": "Четыре",
+        "Five": "Пять",
+        "Six": "Шесть",
+        "Seven": "Семь",
+        "Eight": "Восемь",
+        "Nine": "Девять",
+        "Ten": "Десять"
+    }
+
+
+def num_translate(*args):
+    for i in translate_dict:
+        if num in str(i):
+            print(translate_dict.get(i, 'Значение не найдено'))
+
+
+num = input("Введите значение: ")
+
+num_translate(num)
