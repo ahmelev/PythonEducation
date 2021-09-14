45f748264cbab372a38b2678b0bf516ce3b479dd
ahmelev <ahmelev@gmail.com>
2021-09-15 00:05:57 +0700

    update

--- task_2.py
+++ task_2.py
@@ -2,11 +2,16 @@
 # задания. Примечания: спамер — это клиент, отправивший больше всех запросов; код должен работать даже с файлами,
 # размер которых превышает объем ОЗУ компьютера.
 
+from collections import Counter, defaultdict
+
+"""выводятся все, самый первый в списке наш товарищ, так я и не понял как достать один элемент из 
+полученного типа данных <class 'collections.Counter'>"""
+
 my_real_list = []
 
 with open("nginx_logs.txt", "r", encoding="utf-8") as f:
     for line in f.readlines():
-        my_list = line[:line.find(" ")], line[line.find('"')+1: line.find(" /")], line[line.find(' /')+1: line.find(" HTTP")]
+        my_list = line[:line.find(" ")]
         my_real_list.append(my_list)
-
-print(my_real_list)
+spammer = max(Counter(my_real_list).items(), key=lambda x: x[1])
+print(spammer)
