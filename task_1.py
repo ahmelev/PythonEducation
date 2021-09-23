2e9b253109d72a4f8cf8fcea966d31a4640a3bd5
aleksandrhmelev <ahmelev@gmail.com>
2021-09-23 17:39:18 +0300

    update

--- task_1.py
+++ task_1.py
@@ -13,11 +13,17 @@
 
 # Примечание: подумайте о возможных ошибках в адресе и постарайтесь учесть их в регулярном выражении; имеет ли смысл
 # в данном случае использовать функцию re.compile()?
+import re
 
 
+def email_parse(email_address):
+    parsed = re.findall(r"([^@&]+)@([\w_-][\w_\.-]*\.[\w_-]{2,})$", email_address)
+    if not parsed:
+        raise ValueError(f"Некорректный e-mail: {email_address}")
+    return dict(zip(["username", "domain"], parsed))
 
 
-
+print(email_parse(input("Введите e-mail: ")))
 
 # https://habr.com/ru/post/349860/
 
