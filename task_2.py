867c14b4e1114f7d256acfc17a1c4de15427bec8
ahmelev <ahmelev@gmail.com>
2021-09-28 01:39:24 +0700

    update

---
+++ task_2.py
@@ -0,0 +1,38 @@
+# Реализовать проект расчёта суммарного расхода ткани на производство одежды. Основная сущность (класс) этого проекта
+# — одежда, которая может иметь определённое название. К типам одежды в этом проекте относятся пальто и костюм. У
+# этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и
+# H, соответственно.
+
+# Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 + 0.5), для костюма(
+# 2*H + 0.3). Проверить работу этих методов на реальных данных.
+
+# Выполнить общий подсчёт расхода ткани. Проверить на практике полученные на этом уроке знания. Реализовать
+# абстрактные классы для основных классов проекта и проверить работу декоратора @property.
+
+class WindowDoor:
+    def __init__(self, wd_len, wd_height):
+        self.square = wd_len * wd_height
+
+
+class Room:
+    def __init__(self, len_1, len_2, height):
+        self.square = 2 * height * (len_1 + len_2)
+        self.wd = []
+
+    def add_win_door(self, wd_len, wd_height):
+        self.wd.append(WindowDoor(wd_len, wd_height))
+
+    def common_square(self):
+        main_square = self.square
+        for el in self.wd:
+            main_square -= el.square
+        return main_square
+
+
+r = Room(7, 4, 3.7)
+print(r.square)
+
+r.add_win_door(2, 2)
+r.add_win_door(2, 2)
+r.add_win_door(2, 2)
+print(r.common_square())
