24c1ad1fa9a41c7a7f4a010d606253d9b935582d
ahmelev <ahmelev@gmail.com>
2021-10-06 19:08:24 +0700

    update

--- task_5.py
+++ task_5.py
@@ -15,6 +15,9 @@ class Storage:
         if self.my_store[type]:
             store.my_store.setdefault(type, []).append(self.my_store.setdefault(type).pop(0))
 
+    def __str__(self):
+        return f'В {self.name} на данный момент находится следующее оборудование: {self.my_store}'
+
 
 class OfficeEquipment:
 
@@ -83,24 +86,22 @@ for i in all_office_equipment:
     basic_storage.reception(i)
 
 """ смотрим что на складе """
-print(basic_storage.my_store)
+print(basic_storage)
 print()
-
 """ перемещаем по типу оборудования первую в списке единицу техники в подразделение компании и выводим результат """
 basic_storage.transfer('Scanner', subdivision_one)
+print("Передаем сканер")
+print(subdivision_one)
+print(basic_storage)
 print()
-print(basic_storage.my_store)
-print()
-print(subdivision_one.my_store)
 
 basic_storage.transfer('Copier', subdivision_two)
+print("Передаем ксерокс")
+print(subdivision_two)
+print(basic_storage)
 print()
-print(basic_storage.my_store)
-print()
-print(subdivision_two.my_store)
 
 basic_storage.transfer('Printer', subdivision_three)
-print()
-print(basic_storage.my_store)
-print()
-print(subdivision_three.my_store)
+print("Передаем принтер")
+print(subdivision_three)
+print(basic_storage)
